88a5b2357c55066252deb7038161c7286ed74949
seemless <matthew.christopher.smith@gmail.com>
2013-07-15 08:40:34 -0400

    added upload form. Not working

---
+++ g2recruiting/forms.py
@@ -0,0 +1,13 @@
+__author__ = 'matthew smith'
+
+from wtforms import Form, BooleanField, TextField, FileField, \
+                    validators
+from flask.ext.uploads import UploadSet, DOCUMENTS
+
+docs = UploadSet('resumes',DOCUMENTS)
+
+class RegistrationForm(Form):
+    email = TextField('Email Address', [validators.Length(min=6, max=35)])
+    resume = FileField("Upload your image",
+        validators=[])
+    accept_tos = BooleanField('I accept the TOS', [validators.Required()])
--- g2recruiting/views.py
+++ g2recruiting/views.py
@@ -1,8 +1,26 @@
 __author__ = 'root'
 
 from g2recruiting import app
+from flask import request, redirect, render_template, url_for
+from werkzeug.utils import secure_filename
+
+import forms
 
 @app.route('/test')
 def test():
     return 'hi'
 
+
+@app.route('/login')
+def login():
+    return 'please login'
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = forms.RegistrationForm(request.form)
+    if request.method == 'POST' and form.validate():
+        print (form)
+        file_name = form.resume
+        print(file_name)
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
