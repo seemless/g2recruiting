ebdf8b377c339cd0fcca3d98d76c32f64995ad27
Matt <matthew.christopher.smith@gmail.com>
2013-07-19 14:49:05 -0400

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+__author__ = 'Matthew Smith'
+
+
+import os
+from setuptools import setup
+
+
+setup(
+	name='g2recruiting',
+	version='0.0.1',
+	author='matt smith',
+	description='Provide an external place for resumes to be uploaded.',
+	#packages=find_packages(),
+	packages=['g2recruiting'],
+	zip_safe=True,
+	install_requires=[
+	'Flask>=0.6',
+    'wtforms',
+	]
+
+
+
+)
